04fd6c142e471c31c59f9b7320c9840a67f85e34
Rabsickles91 <grewalrabian@gmail.com>
2023-12-10 16:00:00 -0500

    fixed key error

--- src/Employeelist.jsx
+++ src/Employeelist.jsx
@@ -3,11 +3,11 @@ import data from "./Data";
 
 export default function Employeelist() {
   return (
-   <div className="name" key={Crypto.randomUUID}>
+   <div className="name">
      
      {data.map((person) => (
-      <Link to = {"/employeepage/" + person.id}>
-        <div className="person" key={person.id}>
+      <Link to = {"/employeepage/" + person.id} key={person.id}>
+        <div className="person" >
           <img src= {person.headshot} />
           <h3>{person.name}</h3>
           <h4>{person.title}</h4>
